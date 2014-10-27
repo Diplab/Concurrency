79b00b74b4c5d2b9016173f0b779472c2ae48821
Timmy00274672 <Timmy00274672@gmail.com>
2014-10-27 20:12:41 +0800

    Find the reason why SimplePriorities doesn't work.
    Thread Priorities
    
    The thread priorities available to Java threads on a native threaded JVM
    should be treated as hints to the scheduler, especially if the threads are
    compute-bound. The number of processors available to a process is dynamic and
    unpredictable. Therefore, an attempt to use priorities to schedule execution
    on any multi-tasked, multiprocessor system is not likely to succeed.

--- TimmyNoteOfConcurrency/src/concurrency/SimplePriorities.java
+++ TimmyNoteOfConcurrency/src/concurrency/SimplePriorities.java
@@ -1,5 +1,18 @@
 package concurrency;
 
+/**
+ * It doesn't work.
+ * 
+ * Thread Priorities: The thread priorities available to Java threads on a native
+ * threaded JVM should be treated as hints to the scheduler, especially if the
+ * threads are compute-bound. The number of processors available to a process is
+ * dynamic and unpredictable. Therefore, an attempt to use priorities to
+ * schedule execution on any multi-tasked, multiprocessor system is not likely
+ * to succeed.
+ * 
+ * @author timmy00274672
+ * 
+ */
 public class SimplePriorities extends Thread {
     private int countDown = 5;
     private volatile double d = 0;
